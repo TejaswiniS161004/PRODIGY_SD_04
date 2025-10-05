26709358845b9256d6d183be3a2f59ac773ad9d1
TejaswiniS161004 <rutejaswini@gmail.com>
2025-10-05 20:10:51 +0530

    Task-04: Sudoku Solver using Backtracking

---
+++ sudoku_solver.py
@@ -0,0 +1,69 @@
+# PRODIGY_SD_04 - Sudoku Solver
+# Author: Your Name
+# Method: Backtracking Algorithm
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(board[i][j] if board[i][j] != 0 else ".", end=" ")
+        print()
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+    if num in board[row]:
+        return False
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    box_x = col // 3
+    box_y = row // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def solve(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    row, col = empty
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+sudoku_board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+print("Unsolved Sudoku:")
+print_board(sudoku_board)
+
+if solve(sudoku_board):
+    print("\nSolved Sudoku:")
+    print_board(sudoku_board)
+else:
+    print("No solution exists.")
